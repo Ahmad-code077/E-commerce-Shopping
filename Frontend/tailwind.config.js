7aeec627169564f572cf2831573035f80fbb947c
Ahmad Mamoon <ahmadeveloper077@gmail.com>
2024-12-07 00:28:10 +0500

    Added Optimaize Images

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -9,14 +9,19 @@ export default {
         'custom-900': '900px',
       },
       colors: {
-        primary: '#ed3849',
-        secondary: '#f58f8f',
-        darkCharcoal: '#333333',
-        'primary-dark': '#d23141',
-        'primary-light': '#f4e5ec',
-        'text-dark': '#0f172a',
-        'text-light': '#64748b',
-        'extra-light': '#f8fafc',
+        // Adjusted color scheme
+        primary: '#001D3D', // Navy Blue
+        'primary-dark': '#001029', // A deeper, richer navy for hover states
+        secondary: '#003566', // Blue Complement
+        'secondary-dark': '#002651', // Darker shade for secondary elements
+        boldColor: '#FFD700', // Gold for attention-grabbing elements
+
+        darkCharcoal: '#333333', // Neutral dark for text
+        'darkCharcoal-dark': '#1F1F1F', // An even darker neutral for contrast
+        'primary-light': '#f4e5ec', // Light pinkish
+        'primary-light-dark': '#e8d0da', // Slightly deeper for hover effects
+        'text-dark': '#0f172a', // Dark readable gray
+        'text-dark-muted': '#0d1525', // Subtle variation for inactive elements
       },
     },
   },
