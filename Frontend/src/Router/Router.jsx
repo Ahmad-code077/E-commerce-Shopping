44da07118c7ab3675185890a1abc8ed546ce898f
Ahmad Mamoon <ahmadeveloper077@gmail.com>
2025-04-28 00:24:42 +0500

    fix 6 for vercel.json also for the realod issue

--- Frontend/src/Router/Router.jsx
+++ Frontend/src/Router/Router.jsx
@@ -28,117 +28,122 @@ import {
 } from '../components';
 import AuthenticateUser from '../components/AutenticateUser';
 
-const route = createBrowserRouter([
+const route = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <App />,
+      errorElement: <Error />,
+      children: [
+        {
+          path: '/',
+          element: <Home />,
+        },
+        {
+          path: '/shop',
+          element: <Shop />,
+        },
+        {
+          path: '/shop/:id',
+          element: <SingleShopPage />,
+        },
+        {
+          path: '/pages',
+          element: <Page />,
+        },
+        {
+          path: '/contact',
+          element: <Contact />,
+        },
+        {
+          path: '/cart',
+          element: <Cart />,
+        },
+        {
+          path: '/search',
+          element: <SearchProduct />,
+        },
+        {
+          path: '/categories/:category',
+          element: <SingleCategory />,
+        },
+      ],
+    },
+    {
+      path: '/login',
+      element: <Login />,
+    },
+    {
+      path: '/register',
+      element: <Register />,
+    },
+    {
+      path: '/dashboard',
+      element: <Dashboard />,
+      children: [
+        {
+          path: 'admin', // Admin Route
+          element: (
+            <AuthenticateUser>
+              {' '}
+              <AdminDashboard />{' '}
+            </AuthenticateUser>
+          ),
+        },
+        {
+          path: 'user',
+          element: <UserDashboard />,
+        },
+        {
+          path: 'update-profile',
+          element: <UpdateProfile />,
+        },
+        {
+          path: 'update-password',
+          element: <UpdatePassword />,
+        },
+        {
+          path: 'manage-product', // Admin Route
+          element: (
+            <AuthenticateUser>
+              <ManageProduct />
+            </AuthenticateUser>
+          ),
+        },
+        {
+          path: 'manage-orders', // Admin Route
+          element: (
+            <AuthenticateUser>
+              <ManageOrders />
+            </AuthenticateUser>
+          ),
+        },
+        {
+          path: 'add-new-product', // Admin Route
+          element: (
+            <AuthenticateUser>
+              <AddNewProduct />
+            </AuthenticateUser>
+          ),
+        },
+        {
+          path: 'profile',
+          element: <SeeUserProfile />,
+        },
+        {
+          path: 'payment', // User Payment
+          element: <UserPayment />,
+        },
+        {
+          path: 'orders', // User Orders
+          element: <UserOrders />,
+        },
+      ],
+    },
+  ],
   {
-    path: '/',
-    element: <App />,
-    errorElement: <Error />,
-    children: [
-      {
-        path: '/',
-        element: <Home />,
-      },
-      {
-        path: '/shop',
-        element: <Shop />,
-      },
-      {
-        path: '/shop/:id',
-        element: <SingleShopPage />,
-      },
-      {
-        path: '/pages',
-        element: <Page />,
-      },
-      {
-        path: '/contact',
-        element: <Contact />,
-      },
-      {
-        path: '/cart',
-        element: <Cart />,
-      },
-      {
-        path: '/search',
-        element: <SearchProduct />,
-      },
-      {
-        path: '/categories/:category',
-        element: <SingleCategory />,
-      },
-    ],
-  },
-  {
-    path: '/login',
-    element: <Login />,
-  },
-  {
-    path: '/register',
-    element: <Register />,
-  },
-  {
-    path: '/dashboard',
-    element: <Dashboard />,
-    children: [
-      {
-        path: 'admin', // Admin Route
-        element: (
-          <AuthenticateUser>
-            {' '}
-            <AdminDashboard />{' '}
-          </AuthenticateUser>
-        ),
-      },
-      {
-        path: 'user',
-        element: <UserDashboard />,
-      },
-      {
-        path: 'update-profile',
-        element: <UpdateProfile />,
-      },
-      {
-        path: 'update-password',
-        element: <UpdatePassword />,
-      },
-      {
-        path: 'manage-product', // Admin Route
-        element: (
-          <AuthenticateUser>
-            <ManageProduct />
-          </AuthenticateUser>
-        ),
-      },
-      {
-        path: 'manage-orders', // Admin Route
-        element: (
-          <AuthenticateUser>
-            <ManageOrders />
-          </AuthenticateUser>
-        ),
-      },
-      {
-        path: 'add-new-product', // Admin Route
-        element: (
-          <AuthenticateUser>
-            <AddNewProduct />
-          </AuthenticateUser>
-        ),
-      },
-      {
-        path: 'profile',
-        element: <SeeUserProfile />,
-      },
-      {
-        path: 'payment', // User Payment
-        element: <UserPayment />,
-      },
-      {
-        path: 'orders', // User Orders
-        element: <UserOrders />,
-      },
-    ],
-  },
-]);
+    basename: '/Frontend',
+  }
+);
 
 export default route;
